da4f75d2b4f87e8c41f6fc1a43c3acf20cb65e37
Carlos Alcca <edual986@gmail.com>
2020-11-11 04:35:01 -0300

    Closed #8 entity,model and migrate Turn

---
+++ database/migrations/2020_11_11_073200_create_turns_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTurnsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('turns', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->time('time');
+            $table->boolean('pending');
+            $table->timestamps();
+            $table->foreignId('client_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('turns');
+    }
+}
