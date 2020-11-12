8f8faaf0608c26ad5bb3d8eac54b078e765f83b3
Carlos Alcca <edual986@gmail.com>
2020-11-12 04:04:16 -0300

    Closed #22 - register new Client

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -7,7 +7,7 @@
         </div>
         <div class="mt-5 flex lg:mt-0 lg:ml-4">
             <span class="sm:ml-3 shadow-sm rounded-md">
-                <a href="#" id="Agregar">
+                <a href=" {{ route( 'clients.create' ) }} " id="Agregar">
                     <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700 transition duration-150 ease-in-out">
                         <svg class="-ml-1 mr-2 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"></path>
