c5989ccc73c554a186a4c372c11750fb43f1d052
Carlos Alcca <edual986@gmail.com>
2020-11-12 02:28:48 -0300

    Closed #16 MVC home page turnos

---
+++ app/Http/Controllers/MainpageController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Turn;
+use Illuminate\Http\Request;
+
+class MainpageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $turns = Turn::with('Client')->get();
+        return view('welcome',['turns'=>$turns]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Turn  $turn
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Turn $turn)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Turn  $turn
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Turn $turn)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Turn  $turn
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Turn $turn)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Turn  $turn
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Turn $turn)
+    {
+        //
+    }
+}
