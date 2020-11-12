fd8fb293a06666dbf5544f850bbe953a4afb84f5
Carlos Alcca <edual986@gmail.com>
2020-11-12 04:47:23 -0300

    Closes #26 -Add labels menu bar

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -11,9 +11,12 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">                    
+                    <x-jet-nav-link href="{{ route('clients.index') }}" :active="request()->routeIs('clients.*')">
+                        Clientes
+                    </x-jet-nav-link>
+                    <x-jet-nav-link href="#" :active="request()->routeIs('turns.*')">
+                        Turnos
                     </x-jet-nav-link>
                 </div>
             </div>
@@ -118,7 +121,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                Clientes
             </x-jet-responsive-nav-link>
         </div>
 
