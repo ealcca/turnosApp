18451d80efa280fac7e1565d2875da59323a3f5d
ealcca <edual986@gmail.com>
2020-11-25 15:35:53 -0300

    testing create client (#86) [Closes #84]

--- tests/Feature/ClientTest.php
+++ tests/Feature/ClientTest.php
@@ -16,11 +16,76 @@ class ClientTest extends TestCase
      *
      * @return void
      */
-    public function testViewClientsAsUser()
+    public function testViewClients()
     {
         $user = User::factory()->create();
         $response = $this->actingAs($user)
             ->get('/clients');
         $response->assertStatus(200);
+    }
+
+    public function testCreateClient()
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)
+            ->get(route('clients.create'));
+        $response->assertStatus(200);
+    }
+
+    public function testStoreClient()
+    {   
+        
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->post('clients',[
+            'name' => 'Josefina',
+            'lastname' => 'Sanchez',
+            'age' => 25,
+            'phone' => '248596352'
+        ]);
+        $client = Client::first();
+        $this->assertEquals($client->name,'Josefina');
+        $this->assertEquals($client->lastname,'Sanchez');
+        $this->assertEquals($client->age,25);
+        $this->assertEquals($client->phone, '248596352');
+    }
+
+    public function testViewEditClient()
+    {
+        $client = Client::factory()->create();
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)
+            ->get('clients/'.$client->id.'/edit/');
+        $response->assertStatus(200);
+        $response->assertSee($client->name);
+        $response->assertSee($client->lastname);
+        $response->assertSee($client->age);
+        $response->assertSee($client->phone);   
+    }
+
+    public function testEditClient()
+    {   
+        $user = User::factory()->create();
+        $client = Client::factory()->create();
+        $response = $this->actingAs($user)->put('clients/'.$client->id,[
+            'name' => 'Josefina',
+            'lastname' => 'Sanchez',
+            'age' => 25,
+            'phone' => '248596352'
+        ]);
+        $client = Client::first();
+        $this->assertEquals($client->name,'Josefina');
+        $this->assertEquals($client->lastname,'Sanchez');
+        $this->assertEquals($client->age,25);
+        $this->assertEquals($client->phone,'248596352');
+    }
+
+    public function testDestroyClient()
+    {   
+        $user = User::factory()->create();
+        $client = Client::factory()->create();
+        $response = $this->actingAs($user)
+            ->delete('clients/'.$client->id);
+        $client = Client::all();
+        $this->assertEquals($client->count(),0);        
     }
 }
