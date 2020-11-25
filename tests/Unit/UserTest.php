82accb0f99782eb6adfef5c916d4ea3653cc449b
Carlos Alcca <edual986@gmail.com>
2020-11-25 16:03:51 -0300

    add unit test

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Client;
+use App\Models\Turn;
+use App\Models\User;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testUserIsManager()
+    {
+        $user = User::factory()->create([
+           'role' => 'manager']);
+        $userIsManager = $user->isManager();
+        $this->assertTrue($userIsManager);
+    }
+
+    public function testUserIsNotManager()
+    {
+        $user = User::factory()->create([
+           'role' => 'user']);
+        $userIsManager = $user->isManager();
+        $this->assertFalse($userIsManager);
+    }
+
+
+}
