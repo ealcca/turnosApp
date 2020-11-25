bae1e4c1a7fd1c16ba5985d00d36c27e7f06dd6f
Carlos Alcca <edual986@gmail.com>
2020-11-25 16:03:51 -0300

    add unit test

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,9 @@ class User extends Authenticatable
         return $this->hasMany(Turn::class);
     }
 
+    public function isManager()
+    {
+        return $this->attributes['role'] == 'manager';
+    }
+
 }
--- tests/Unit/TurnTest.php
+++ tests/Unit/TurnTest.php
@@ -10,17 +10,19 @@ use Tests\TestCase;
 class TurnTest extends TestCase
 {
     
-    public function testClient()
-    {
-        $client = Client::factory()->create();
-        $turn = Turn::factory()->create(['client_id'=>$client]);
-        $this->assertEquals($client->id,$turn->client->id);
-    }
-
     public function testUser()
     {
         $user = User::factory()->create();
         $turn = Turn::factory()->create(['user_id'=>$user]);
         $this->assertEquals($user->id,$turn->user->id);
     }
+
+    public function sa()
+    {
+        $user = User::factory()->create();
+        $turn = Turn::factory()->create(['user_id'=>$user]);
+        $this->assertEquals($user->id,$turn->user->id);
+    }
+
+
 }
